d40c50effa1c0055cae2f40bcc6adb96b77dd747
Charles <cchester1@angelo.edu>
2022-02-03 14:58:36 -0600

    initial on mac

--- javascript/getData.js
+++ javascript/getData.js
@@ -1,12 +1,11 @@
 const search = document.querySelector('#searchBtn')
-const longitude = document.getElementById('longitude')
+const longitude = document.getElementById('longitude').value
 const latitude = document.getElementById('latitude').value
 const date = document.getElementById("date").value
 const tz = document.getElementById('tz').value
-const url = 'https://api.solunar.org/solunar/42.66,-84.07,20180207,-4'
 
-async function fetchUrl() {
-    await fetch('https://api.solunar.org/solunar/42.66,-84.07,20180207,-4')
+function fetchUrl() {
+    fetch(`https://api.solunar.org/solunar/${longitude},${latitude},${date},${tz}`)
         .then(results => {
             return results.json()
         })
@@ -15,6 +14,4 @@ async function fetchUrl() {
         })
 }
 
-search.addEventListener('click', console.log(url))
-
-// ${url}${latitude},${longitude},${date},${tz}
+search.addEventListener('click', fetchUrl)
