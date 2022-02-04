0ef292ec1c80c466459044ffde94a76c0a861e33
Charles <cchester1@angelo.edu>
2022-02-04 08:06:37 -0600

    working on localStorage feature within getData.js

--- public/javascript/getData.js
+++ public/javascript/getData.js
@@ -2,12 +2,13 @@ const longitude = document.getElementById('longitude').value
 const latitude = document.getElementById('latitude').value
 const date = document.getElementById("date").value
 const tz = document.getElementById('tz').value
+let searchBtn = document.querySelector('#searchBtn')
 // const section = document.getElementById('section')
 
 function fetchUrl() {
     fetch('https://api.solunar.org/solunar/' + longitude + ',' + latitude + ',' + date + ',' + tz, {
         method: "GET",
-        headers:{"Content-Type": "application/JSON"}
+        headers: { "Content-Type": "application/JSON" }
     })
         .then(results => {
             return results.json()
@@ -18,4 +19,19 @@ function fetchUrl() {
         })
 }
 
+function saveTask() {
+    localStorage.setItem('longitude', JSON.stringify(longitude))
+    localStorage.setItem('latitude', JSON.stringify(latitude))
+    localStorage.setItem('date', JSON.stringify(date))
+    localStorage.setItem('tz', JSON.stringify(tz))
+}
+
+function loadTask() {
+    JSON.parse(localStorage.getItem('longitude'))
+    JSON.parse(localStorage.getItem('latitude'))
+    JSON.parse(localStorage.getItem('date'))
+    JSON.parse(localStorage.getItem('tz'))
+}
+
+searchBtn.addEventListener('submit', saveTask)
 
