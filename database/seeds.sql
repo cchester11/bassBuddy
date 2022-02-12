5a3fccdd21995303af233cc77dc08a28738db03c
Charles <cchester1@angelo.edu>
2022-02-12 11:38:49 -0600

    ..

---
+++ database/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO catches (catch_title, catch_type, 
+season,
+catch_description)
+
+VALUES (
+      'catch one',
+      'large mouth',
+      'spring',
+      'catch was close to the bank. used a lunker log worm across the floor'
+      )
+      (
+      'catch two',
+      'large mouth',
+      'winter',
+      'catch was deep. used a deep lipped crankbait'
+      )
+      (
+      'catch three',
+      'large mouth',
+      'summer',
+      'fish was dug into a cut of weeds. Used a frog to invoke an aggressive bite'
+      )
