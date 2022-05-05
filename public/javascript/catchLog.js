5afb9b72f7f6dac74faa2f8312d7322e340eca08
Charles <cchester1@angelo.edu>
2022-05-05 15:02:52 -0500

    getting ready to list each catch on catch log page

--- public/javascript/catchLog.js
+++ public/javascript/catchLog.js
@@ -1,13 +1,16 @@
-const createDiv = document.createElement('div')
-const createP = document.createElement('p')
-
 async function renderAllCatches () {
       await fetch('/api/getallcatches', {
             method: "GET",
             headers: ({ 'Content-Type': 'application/json'})
       })
             .then(response => response.json())
-            .then(data => console.log(data.rows))
+            .then(data => {
+                  let fish = data.rows
+                  
+                  for(let i = 0; i < fish.length; i ++) {
+                        console.log(fish[i])
+                  }
+            })
             .catch(err => {
                   if(err) {
                         throw new Error(err)
