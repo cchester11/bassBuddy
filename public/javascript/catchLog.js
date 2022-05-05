14d38b6f6a6774374924372904dc5e72eb594ac9
Charles <cchester1@angelo.edu>
2022-05-05 16:13:12 -0500

    catches successfully rendered on catch log page. need to style the page now

--- public/javascript/catchLog.js
+++ public/javascript/catchLog.js
@@ -1,3 +1,6 @@
+// use catchLogDiv
+const catchLogDiv = document.getElementById('catchLogDiv')
+
 async function renderAllCatches () {
       await fetch('/api/getallcatches', {
             method: "GET",
@@ -9,6 +12,25 @@ async function renderAllCatches () {
                   
                   for(let i = 0; i < fish.length; i ++) {
                         console.log(fish[i])
+
+                        const catchDiv = document.createElement('div')
+
+                        let title = document.createElement('h5')
+                        title.textContent = fish[i].catch_title
+                        let species = document.createElement('ul')
+                        species.textContent = fish[i].catch_type
+                        let season = document.createElement('ul')
+                        season.textContent = fish[i].season
+                        let description = document.createElement('ul')
+                        description.textContent = fish[i].catch_description
+
+                        catchDiv.setAttribute('style', "d-flex justify-content-center align-items-start m-5")
+                        catchDiv.appendChild(title)
+                        catchDiv.appendChild(species)
+                        catchDiv.appendChild(season)
+                        catchDiv.appendChild(description)
+
+                        catchLogDiv.appendChild(catchDiv)
                   }
             })
             .catch(err => {
