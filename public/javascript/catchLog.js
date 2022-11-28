9a4211dc3f5d0cb2b720ceab10070efba731a5b5
Charles <cchester1@angelo.edu>
2022-11-28 15:39:24 -0600

    #14 simple redesign finished

--- public/javascript/catchLog.js
+++ public/javascript/catchLog.js
@@ -1,20 +1,19 @@
-// use catchLogDiv
 const catchLogDiv = document.getElementById('catchLogDiv')
+const betaDiv = document.getElementById('betaDiv')
 
-async function renderAllCatches () {
+async function renderAllCatches() {
       await fetch('/api/getallcatches', {
             method: "GET",
-            headers: ({ 'Content-Type': 'application/json'})
+            headers: ({ 'Content-Type': 'application/json' })
       })
             .then(response => response.json())
             .then(data => {
                   let fish = data.rows
-                  
-                  for(let i = 0; i < fish.length; i ++) {
+
+                  for (let i = 0; i < fish.length; i++) {
                         console.log(fish[i])
                         // create cards for each catch (see dataReader for specs)
-                        const card = document.createElement('div')
-                        const catchDiv = document.createElement('div')
+                        const colDiv = document.createElement('div')
 
                         let title = document.createElement('h5')
                         title.textContent = fish[i].catch_title
@@ -24,25 +23,18 @@ async function renderAllCatches () {
                         season.textContent = fish[i].season
                         let description = document.createElement('ul')
                         description.textContent = fish[i].catch_description
+                        colDiv.setAttribute('class', 'col p-3')
 
-                        card.setAttribute('class', 'card')
-                        catchDiv.setAttribute('class', "card-body ")
-                        title.setAttribute('class', 'card-title')
-                        species.setAttribute('class', 'card-text')
-                        season.setAttribute('class', 'card-text')
-                        description.setAttribute('class', 'card-text')
-                        
-                        card.appendChild(catchDiv)
-                        catchDiv.appendChild(title)
-                        catchDiv.appendChild(species)
-                        catchDiv.appendChild(season)
-                        catchDiv.appendChild(description)
-
-                        catchLogDiv.appendChild(catchDiv)
+                        colDiv.appendChild(title)
+                        colDiv.appendChild(species)
+                        colDiv.appendChild(season)
+                        colDiv.appendChild(description)
+                        betaDiv.appendChild(colDiv)
+                        catchLogDiv.appendChild(betaDiv)
                   }
             })
             .catch(err => {
-                  if(err) {
+                  if (err) {
                         throw new Error(err)
                   }
             })
